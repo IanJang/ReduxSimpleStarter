b110395a0532dd06754ec6ee3f90474a9e35e168
ianjang <ianjang0718@gmail.com>
2021-03-20 21:44:10 +0900

    Application Styling

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,11 +4,11 @@ import ReactDOM from 'react-dom';
 class SearchBar extends Component {
     constructor(props) {
         super(props);
-        this.state = { term: 'Starting Value' };
+        this.state = { term: '' };
     }
     render() {
         return (
-            <div>
+            <div className="search-bar">
                 <input
                     value={this.state.term}
                     onChange={event => this.setState({term: event.target.value})} />
